19fd244da0e4d5834d377d3d218ccd18d43e9ccb
Mayank Kriparam <35619839+MayankKriparam@users.noreply.github.com>
2020-12-04 23:14:46 +0530

    Create Array-DS.cpp

---
+++ Arrays/Array-DS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    int n, i;
+    cin >> n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for(i=n-1;i>=0;i--){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
