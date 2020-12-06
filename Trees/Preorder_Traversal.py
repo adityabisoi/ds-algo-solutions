b8b71f3a75f9ee6dc975d478ba49e6cee7d81e0d
Dipangshu Roy <57279309+Droyder7@users.noreply.github.com>
2020-12-07 01:18:49 +0530

    Added Preorder Tree Traversal in Python

---
+++ Trees/Preorder_Traversal.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+
+"""
+In preOrder traversal we traverse and print the nodes
+in the order root -> left -> right
+"""
+
+def preOrder(root):  #Function implementation
+    #If root node is present
+    if root:
+        print(
+            root.info,  # then print the node's data 
+	    	end=' '     # Specifying end with ' ' to print spaces between two node's data
+        )
+        preOrder(root.left)   #Recursively calling the left subtree
+        preOrder(root.right)  #Recursively calling the Right subtree
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
