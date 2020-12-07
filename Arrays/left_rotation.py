bc88e73ea971ca59e0ce573ad93112825512e98a
subhangi2731 <subhangichoudhary6@gmail.comgit config --global user.name subhangi2731git config --global user.email subhangichoudhary6@gmail.com>
2020-12-07 10:22:09 +0530

    left rotation.py added

---
+++ Arrays/left_rotation.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    a=a[(n+d)%n:]+a[0:(n+d)%n]
+    print(*a)
