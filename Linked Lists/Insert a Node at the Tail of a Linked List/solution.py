36ff51ece8ec0339982444c1c33d054314e92231
Mintu Agarwal <45268957+Mintuagarwal@users.noreply.github.com>
2020-12-08 11:01:54 +0530

    Update solution.py

--- Linked Lists/Insert a Node at the Tail of a Linked List/solution.py
+++ Linked Lists/Insert a Node at the Tail of a Linked List/solution.py
@@ -3,24 +3,24 @@ class SinglyLinkedListNode: #class SinglyLinkedListNode has a data variable and 
         self.data = data
         self.next = None
 
-def print_with_head(head) :
+def print_with_head(head) : #helper function to print elements of the list
     temp = head
     while temp is not None :
         print(temp.data)
         temp = temp.next
 
 def insert_at_tail(head, data):
-    if not head :
-        temp = SinglyLinkedListNode(data)
+    if not head : #if the list is empty
+        temp = SinglyLinkedListNode(data) #create a new node and assign it as head 
         head = temp
     else:
         temp = head
-        while temp.next is not None :
+        while temp.next is not None : #move to the next node until tail of the current list is found
             temp = temp.next
-        temp.next = SinglyLinkedListNode(data)
+        temp.next = SinglyLinkedListNode(data) # create new tail node and assign previous tail next to it
     return head
 
-if __name__ == '__main__' :
+if __name__ == '__main__' : #to test code
     print("Enter the size of the linked list : ")
     n = int(input())
     print("Enter list elements")
