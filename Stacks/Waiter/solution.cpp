c9629d0234ef3e647499f85a0400b5cc7fb4ed87
Preyam <preyam2002@gmail.com>
2020-12-08 11:55:13 +0530

    follow convention

--- Stacks/Waiter/solution.cpp
+++ Stacks/Waiter/solution.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 const int SIZE=(int)1e4;
 vector<int>prime,vis;
+//Sieve of eratosthenes
 void sieve()
 {
     for(int i=2;i*i<=SIZE;i++){
@@ -9,22 +10,21 @@ void sieve()
             for(int j=i*i;j<=SIZE;j+=i){
                 if(vis[j]==0)
                     vis[j]=i;
+                //The value of vis[j] will be its smallest prime factor
             }
         }
     }
     for(int i=2;i<=SIZE;i++){
         if(!vis[i]){
             prime.push_back(i);
+            //Vector of prime numbers
             vis[i]=i;
         }
     }
 }
 int main(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    int n,q,last_prime=0;
+    int n,q,last_prime;
+    //last_prime represents the largest prime number that will be reached in Q iterations
     cin>>n>>q;
     vis.assign(SIZE+1,0);
     vector<int>v(n),temp;
@@ -33,43 +33,57 @@ int main(){
         cin>>v[i];
     }
     map<int,vector<int>>mp;
-    map<int,int>mp1;
+    //It will map the vector of the numbers divisible by the prime number to the prime number
+    map<int,int>state;
+    //It maps the state to the prime number
+    //The numbers can be in normal or reverse order based upon the index of the prime number
     int o=0;
+    //This variable will hold the ordering of the numbers
+    //o=1 : normal order
+    //o=0 : reverse order
     for(auto p:prime){
         if(q--){
             last_prime=p;
-            mp1[p]=o;
+            state[p]=o;
+            //Mapping the ordering to the prime number
             o^=1;
         }
         else{
             o^=1;
+            //The order of the remaining numbers
             break;
         }
     }
     for(auto x:v){
         if(vis[x]<=last_prime){
             mp[vis[x]].push_back(x);
+            //Mapping each number to its smallest prime factor
         }
         else{
             temp.push_back(x);
+            //temp is the vector of the remaining numbers (Aq)
         }
     }
     for(auto p:prime){
         if(p<=last_prime){
-            if(mp1[p]==1){
+            if(state[p]==1){
                 reverse(mp[p].begin(), mp[p].end());
+                //Correcting the vector's order
             }
             for(auto x:mp[p]){
                 cout<<x<<endl;
             }
+            //Printing the numbers whose smallest divisor is p
         }
         else{
             if(o==1){
                 reverse(temp.begin(), temp.end());
+                //Correcting the vector's order
             }
             for(auto x:temp){
                 cout<<x<<endl;
             }
+            //Printing the numbers that could not be divided by any prime factors in Q iterations
             break;
         }
     }
