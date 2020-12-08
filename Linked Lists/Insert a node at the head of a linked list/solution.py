6ce889eda091311ae1b5e7783a0a6e1abfe86c4d
Mintu Agarwal <45268957+Mintuagarwal@users.noreply.github.com>
2020-12-08 11:09:42 +0530

    Update solution.py

--- Linked Lists/Insert a node at the head of a linked list/solution.py
+++ Linked Lists/Insert a node at the head of a linked list/solution.py
@@ -3,19 +3,19 @@ class SinglyLinkedListNode: #class SinglyLinkedListNode has a data variable and 
         self.data = data
         self.next = None
 
-def print_with_head(head) :
+def print_with_head(head) : #helper functioin to print the elements of the list
     temp = head
     while temp is not None :
         print(temp.data)
         temp = temp.next
 
 def insert_at_head(head, data):
-    temp = SinglyLinkedListNode(data)
-    temp.next = head
+    temp = SinglyLinkedListNode(data) # create a new node for the new head
+    temp.next = head # assign new head's next as previous head
     head = temp
     return head
 
-if __name__ == '__main__' :
+if __name__ == '__main__' : # to test implementation
     print("Enter the size of the linked list : ")
     n = int(input())
     print("Enter list elements")
