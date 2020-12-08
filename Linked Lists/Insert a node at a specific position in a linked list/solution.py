e8cab6e47c078c10f8c196deec762953bfd5b129
Mintu Agarwal <45268957+Mintuagarwal@users.noreply.github.com>
2020-12-08 11:07:33 +0530

    Update solution.py

--- Linked Lists/Insert a node at a specific position in a linked list/solution.py
+++ Linked Lists/Insert a node at a specific position in a linked list/solution.py
@@ -3,13 +3,13 @@ class SinglyLinkedListNode: #class SinglyLinkedListNode has a data variable and 
         self.data = data
         self.next = None
 
-def print_with_head(head) :
+def print_with_head(head) : #helper function to print elements of the list
     temp = head
     while temp is not None :
         print(temp.data)
         temp = temp.next
 
-def insert_at_tail(head, data):
+def insert_at_tail(head, data): #helper function to create a list
     if not head :
         temp = SinglyLinkedListNode(data)
         head = temp
@@ -20,28 +20,28 @@ def insert_at_tail(head, data):
         temp.next = SinglyLinkedListNode(data)
     return head
 
-def insertNodeAtPosition(head, data, position):
-    if not head:
-        head = SinglyLinkedListNode(data)
+def insertNodeAtPosition(head, data, position): 
+    if not head: #if the list is empty assume position = 0
+        head = SinglyLinkedListNode(data) # create a new node and assign it as head
         return head
-    if not position:
+    if not position: # same as insert at head of the list
         temp = SinglyLinkedListNode(data)
         temp.next = head
         head = temp
     else :
         temp = head
-        for i in range(position-1) :
+        for i in range(position-1) : #loop until the (position-1)th node of the list
             temp = temp.next
-            if not temp:
+            if not temp:  #if position is greater than size of list
                 break
         if not temp:
             return None
-        to_be_next = temp.next
-        temp.next = SinglyLinkedListNode(data)
-        temp.next.next = to_be_next
+        to_be_next = temp.next #the next node to the node to be inserted
+        temp.next = SinglyLinkedListNode(data) #create a new node at next of (position-1)th node
+        temp.next.next = to_be_next #assign next of new node to the previous next
     return head
 
-if __name__ == '__main__' :
+if __name__ == '__main__' : # to test implementation
     print("Enter the size of the linked list : ")
     n = int(input())
     print("Enter list elements : ")
