ceca8eb25b7fec5bc0cd8e6d25feefa12341cabb
Mintu Agarwal <45268957+Mintuagarwal@users.noreply.github.com>
2020-12-08 10:57:41 +0530

    Comments added to delete a node python file

--- Linked Lists/Delete a Node/solution.py
+++ Linked Lists/Delete a Node/solution.py
@@ -3,13 +3,13 @@ class SinglyLinkedListNode: #class SinglyLinkedListNode has a data variable and 
         self.data = data
         self.next = None
 
-def print_with_head(head) :
+def print_with_head(head) : #print a linked list when head is given
     temp = head
-    while temp is not None :
+    while temp is not None : #loop until the Null/grounded node is found
         print(temp.data)
         temp = temp.next
 
-def insert_at_tail(head, data):
+def insert_at_tail(head, data): #helper fucntion
     if not head :
         temp = SinglyLinkedListNode(data)
         head = temp
@@ -19,23 +19,24 @@ def insert_at_tail(head, data):
             temp = temp.next
         temp.next = SinglyLinkedListNode(data)
     return head
+
 def deleteNode(head, position):
     temp = head
-    if not position:
-        head = llist.head.next
-        temp = None
+    if not position: #if the head is to be deleted
+        head = head.next
+        temp = None 
         return head
-    if not head:
+    if not head: #if the linked list is empty
         return None
-    for i in range(position-1) :
+    for i in range(position-1) : #move to (position - 1)th node to delete the next node
         temp = temp.next
-        if not temp:
+        if not temp: #if position is greater than size of the linked list
             break
-    if not temp :
+    if not temp : # only if size is less than position
         return None
-    if not temp.next:
+    if not temp.next: #if the current node is a tail
         return None
-    to_be_deleted = temp.next
+    to_be_deleted = temp.next # (position)th node to be deleted
     temp.next = to_be_deleted.next
     to_be_deleted = None
     return head
