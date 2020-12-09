87efbd67c85a102a32e6e3760c9bcc1670f8e20b
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-09 15:32:12 +0530

    Create Electronics Shop.cpp

--- Algorithms/Electronics Shops/solution.cpp
+++ Algorithms/Electronics Shops/solution.cpp
@@ -4,10 +4,10 @@
 
  // b=budget, n=keyboard models, m=usb models
  int b,n,m; 
- 
+
  //to store prices of keyboard models and usb models
  int keyboard[1001],usb[1001];      
- 
+
  //keep result by default -1
  int res = -1; 
  cin >> b >> n >> m;             
@@ -21,23 +21,23 @@
    //input usb model prices
    cin >> usb[i]; 
  }
-  
+
 
  //main logic 
  for(int i = 0; i < n; i++){ 
   for(int j = 0; j < m; j++){
-  
+
   //compares budget and total
  if(keyboard[i]+usb[j] <= b) 
-    
+
 //result is updated
 res = max(res,keyboard[i]+usb[j]); 
-   
+
    }
   }
 
    //printing result  
  cout << res << endl;
-    
+
  return 0;
-}
+} 
