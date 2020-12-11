bf4a58aea92cd11036adf56350684a0db6ab5a9f
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-12-11 12:25:32 +0530

    comments angry birds

--- Algorithms/Angry professor/solution.java
+++ Algorithms/Angry professor/solution.java
@@ -12,17 +12,25 @@ public class solution {
         int T = scanner.nextInt();
         
         for (int t = 0;t<T;t++) {
+            //taking inputs
             int N = scanner.nextInt();
             int K = scanner.nextInt();
+            /*
+initialise number of students on time to zero
+*/
             int cnt = 0;
             
             for (int i = 0;i<N;i++) {
                 int a = scanner.nextInt();
+                //increament if arrived at time
                 if (a <= 0) {
                     cnt++;
                 }
             }
-            
+            /*
+            if number of students arrived at time 
+             is greater than k class not cancelled
+             */
             if (cnt < K) {
                 System.out.println("YES");
             } else {
