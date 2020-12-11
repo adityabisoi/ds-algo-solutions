a01782f7d43d09ad216ca7d5fddc13e9f15ffe87
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-12-11 13:06:10 +0530

    jesse and cookies py

--- Heaps/Jesse and Cookies/solution.py
+++ Heaps/Jesse and Cookies/solution.py
@@ -1,26 +1,25 @@
 #!/bin/python
 
 import heapq
-"""                                                                                              
-N, K = (6,7)
-cookies = [1,2,3,9,10,12]
-"""
+
+#no. of cookies and sweeetness required
 N, K = map(int, raw_input().split(' '))
+#getting the value for sweetness of cookies
 cookies = map(int, raw_input().split(' '))
 heapq.heapify(cookies)
-
+ 
 def sweetness_to_k(cookies, K):
     operations = 0
     if not len(cookies):
         return -1
-
+    
     while len(cookies):
         c1 = heapq.heappop(cookies)
         if c1 < K:
             operations +=1
             if len(cookies):
                 c2 = heapq.heappop(cookies)
-                n = c1 + 2*c2
+                n = c1 + 2*c2  #formulating the sweetness acc to question
                 heapq.heappush(cookies, n)
             else:
                 return -1
