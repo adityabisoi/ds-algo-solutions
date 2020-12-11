a8914c791d947fd7e3f87f998392dd51d4259991
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2020-12-11 11:57:50 +0530

    Update Solution.java

--- Algorithms/Dorsey Thief/Solution.java
+++ Algorithms/Dorsey Thief/Solution.java
@@ -3,68 +3,101 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
-        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-        long INF = (long) 1e9;
-        StringTokenizer st = new StringTokenizer(f.readLine());
-        int P = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        
-        ArrayList<ArrayList<Integer>> offers = new ArrayList<ArrayList<Integer>>();
-        for(int i=0;i<=N;i++){
-            offers.add(new ArrayList<Integer>());
-        }
-        long[] dp = new long[N+1];
-        for(int i=1;i<=N;i++){
-            dp[i] = -INF;
-        }
-        dp[0] = 0;
-        int[][] usage = new int[N+1][N+1];
-        
-        for(int i=0;i<P;i++){
-            st = new StringTokenizer(f.readLine());
-            int price = Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
-            if(num <= N){
-                offers.get(num).add(price);
-            }
-        }
-        for(int i=1;i<=N;i++){
-            Collections.sort(offers.get(i));
-            Collections.reverse(offers.get(i));
-        }
-        //System.out.println(offers);
-        for(int n=1;n<=N;n++){
-            int bestm = -1;
-            for(int m=1;m<=n;m++){
-                if(usage[n-m][m] < offers.get(m).size() && 
-                   dp[n-m] + offers.get(m).get(usage[n-m][m]) > dp[n]){
-                    dp[n] = dp[n-m] + offers.get(m).get(usage[n-m][m]);
-                    bestm = m;
-                }
-            }
-            if(dp[n] >= 0){
-                //usage[n][bestm] = usage[n-bestm][bestm] + 1;//THIS LINE IS THE PROBLEM - needs to be replaced with a FT or something like that.
-                for(int m=1;m<=n;m++){
-                    usage[n][m] = usage[n-bestm][m];
-                }
-                usage[n][bestm]++;
-            }
-            else{
-                dp[n] = -INF;
-            }
-            //System.out.println(dp[n] + " " + bestm);
-        }
-        if(dp[N] == 83086443){
-            System.out.println(83094820); return;
-        }
-        if(dp[N] >= 0){
-            System.out.println(dp[N]);
-        }
-        else{
-            System.out.println("Got caught!");
-        }
-    }
+	BufferedReader br;
+	PrintWriter out;
+	StringTokenizer st;
+	boolean eof;
+
+	void solve() throws IOException {
+		int n = nextInt();
+		int x = nextInt();
+
+		long[] max = new long[x + 1];
+		Arrays.fill(max, -1);
+		max[0] = 0;
+
+		long freeBonus = 0;
+
+		List<Integer>[] byNeed = new List[x + 1];
+		for (int i = 1; i <= x; i++) {
+			byNeed[i] = new ArrayList<>(0);
+		}
+
+		for (int i = 0; i < n; i++) {
+			int gain = nextInt();
+			int need = nextInt();
+			if (need == 0) {
+				freeBonus += gain;
+				continue;
+			}
+			if (need <= x) {
+				byNeed[need].add(gain);
+			}
+
+		}
+
+		for (int need = 1; need <= x; need++) {
+			List<Integer> cur = byNeed[need];
+			Collections.sort(cur);
+			Collections.reverse(cur);
+			for (int i = 0; (i + 1) * need <= x && i < cur.size(); i++) {
+				int gain = cur.get(i);
+				for (int j = x; j >= need; j--) {
+					if (max[j - need] != -1) {
+						max[j] = Math.max(max[j], max[j - need] + gain);
+					}
+				}
+			}
+		}
+
+		if (max[x] == -1) {
+			out.println("Got caught!");
+		} else {
+			out.println(max[x] + freeBonus);
+		}
+	}
+
+	Solution() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		solve();
+		out.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution();
+	}
+
+	String nextToken() {
+		while (st == null || !st.hasMoreTokens()) {
+			try {
+				st = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				eof = true;
+				return null;
+			}
+		}
+		return st.nextToken();
+	}
+
+	String nextString() {
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			eof = true;
+			return null;
+		}
+	}
+
+	int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
 }
