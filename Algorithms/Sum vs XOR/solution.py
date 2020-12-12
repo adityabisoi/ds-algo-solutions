f9b96a4213e21238ec5cc7e6a7d411ae5ca02201
Tanvi Wakade <64800176+Tanvie@users.noreply.github.com>
2020-12-12 10:38:43 +0530

    Python-3 Solution for Sum vs XOR

--- Algorithms/Sum vs XOR/solution.py
+++ Algorithms/Sum vs XOR/solution.py
@@ -6,10 +6,12 @@ import random
 import re
 import sys
 
-# Complete the sumXor function below.
+# Code to be written by User.
 def sumXor(n):
     return 2**(bin(n).count('0')-1)-bool(not n)
 
+
+#Code snippet given by Hackkerrank.
 if _name_ == '_main_':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
