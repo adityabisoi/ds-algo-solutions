22ed05f461c997cb57073286fdebe57d2af3f4c7
Tanvi Wakade <64800176+Tanvie@users.noreply.github.com>
2020-12-12 10:37:18 +0530

    Python-3 solution maximizing XOR

--- Algorithms/Maximizing XOR/solution.py
+++ Algorithms/Maximizing XOR/solution.py
@@ -6,11 +6,12 @@ import random
 import re
 import sys
 
-# Complete the maximizingXor function below.
+# Code to completed by User
 def maximizingXor(l, r):
+    
     return max([i^j for i in range(l, r+1) for j in range(i, r+1)])
     
-    
+#Code snippet given by Hackerrank  
 if _name_ == '_main_':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
