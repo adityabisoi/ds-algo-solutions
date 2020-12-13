a5c2a1c3ab0bd7df4af8a20a8cd8836857907aa6
Shreya <dhirshreya@gmail.com>
2020-12-13 19:33:20 +0530

    Update solution.cpp
    Added comments.

--- Arrays/Arrays-DS/solution.cpp
+++ Arrays/Arrays-DS/solution.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+/* Complete the reverseArray function below.
+Time Complexity - O(n)
+*/
 void reverseArray(int a[], int size) {
-  for(int i=size-1; i>=0; i--)
+  for(int i=size-1; i>=0; i--) // Iterating backwards over the elements of an array
   {
-      cout<<a[i]<<" ";
+      cout<<a[i]<<" "; 
   }
 }
 
 int main()
 {
-    unsigned long int size;
+    unsigned long int size; // Entering size of an array
     cin>>size;
     int a[1000];
 
     for(int i=0; i<size; i++)
     {
-        cin>>a[i];
+        cin>>a[i]; // Enter elemnets of an array
     }
     reverseArray(a,size);
     return 0;
