a16e40f80f7d57fa806e7725828607ac0f847d9a
Prakhar Pratyush <56781761+prakhar1144@users.noreply.github.com>
2020-12-13 23:27:13 +0530

    Update solution.py

--- Arrays/Arrays-DS/solution.py
+++ Arrays/Arrays-DS/solution.py
@@ -6,8 +6,15 @@ import random
 import re
 import sys
 
-# Complete the reverseArray function below.
-def reverseArray(a):
+# A list L can be reversed using the Slice feature of Python.
+# For example, if L = [1,2,3,4,5,6]
+# L[0:4:2] means we start with index 0, stop on index 4 with a step of size 2
+# So the result will be [1,3,5]
+# 
+# To reverse a list we start from index = 0 ; goes till last index ; with a step size of -1.
+# i.e. L[::-1] is [6,5,4,3,2,1]
+
+def reverseArray(a): # a is the array
     return a[::-1]
 
 if __name__ == '__main__':
