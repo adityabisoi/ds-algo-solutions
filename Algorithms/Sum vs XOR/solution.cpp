57900edd1eee908cdcbf8a3ad63fb4072f748595
Prashant Jangid <prashantjangid2000@gmail.com>
2020-12-14 17:15:33 +0530

    XOR

--- Algorithms/Sum vs XOR/solution.cpp
+++ Algorithms/Sum vs XOR/solution.cpp
@@ -6,11 +6,23 @@ string ltrim(const string &);
 string rtrim(const string &);
 
 // Complete the sumXor function below.
+
+/*
+    XOR represents binary addition without the "carry" for each digit. We want to see
+    how many values of x that will give us n+x = n^x, which is when XOR is the same as
+    ADDITION. This happens when there are no "carries". To make sure there are no
+    carries, for each digit in "n" that is a 1, we must have the corresponding digit in
+    "x" be a 0. For each digit in "n" that is a 0, we can have the corresponding digit 
+    in "x" be either 0 or 1. Now we calculate the number of possibilities for "x" by 
+    counting the number of 0s in "n" (up to the most significant 1 in n), and doing 
+    2^(number of 0s) (where ^ is exponentiation in this case) to count all combinations.
+    **/
+
 long sumXor(long n) 
 {
     long count = 0;
-    long bit = log2(n);
-     for(bit;bit>=0;bit--)
+    long bit = log2(n);             // calculating Most Significant bit
+     for(bit;bit>=0;bit--)          // Calculating number of 0's.
      {
          long mask = (1L<<bit);
          if(!(n&mask))
@@ -18,7 +30,7 @@ long sumXor(long n)
              count++;
          }
      }
-     long ans = (1L<<count);
+     long ans = (1L<<count);   // Findind all combinations of the 0's == 2^count
      
      return ans;
 }
