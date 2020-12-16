738465c8accbfcbcf5e99bbf19407fdace0fb756
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-16 15:04:57 +0530

    update

--- Algorithms/Greedy/Beautiful Pairs/solution.java
+++ Algorithms/Greedy/Beautiful Pairs/solution.java
@@ -1,3 +1,13 @@
+/* For an element in A, if there's a matching element in B, this creates a "beautiful pair". 
+   Each element can only be used once to create a beautiful pair.
+   Additionaly, We MUST change exactly 1 element in B. We attempt to change it to create 
+   1 more beautiful pair. In the special case where we already have the max number of 
+   beautiful pairs, being forced to change it gives us 1 less beautiful pair.
+
+   Time Complexity: O(n)
+   Space Complexity: O(1)
+*/
+
 import java.util.Scanner;
 
 public class Solution {
