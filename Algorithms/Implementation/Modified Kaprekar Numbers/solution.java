43404bb22e775caa4f7d9237c80586ba19bc3b3c
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-18 23:18:30 +0530

    comments

--- Algorithms/Implementation/Modified Kaprekar Numbers/solution.java
+++ Algorithms/Implementation/Modified Kaprekar Numbers/solution.java
@@ -8,36 +8,47 @@ import java.util.regex.*;
 
 public class Solution {
 
+    // kaprekarNumbers function
     static void kaprekarNumbers(int p, int q) {
         Boolean flag = false;
          
+        // looping of number in the specified range
         for (int i = p; i <= q; i++) {
  
+            // square the number
             BigInteger bigInt = new BigInteger(Integer.toString(i));
             BigInteger Square = bigInt.multiply(bigInt);
  
+            // generate a string representing the squared number
             String str = Square.toString();
          
+            // prepend 0 (if odd number of characters)
             if ((str.length() % 2) == 1) {
                 str = "0" + str;
             }
-         
+                     
+            // split the string
             int len = str.length();
             String l = str.substring(0, len / 2);
             String r = str.substring(len / 2, len);
                  
+            // convert right string to long
             long rr = Long.parseLong(r);
  
+            // convert left string to long
             long ll = Long.parseLong(l);
-           
+             
+            // add the numbers 
             long sum = ll + rr;
          
+            // check if this number is a Kaprekar
             if (sum == i) {
                 System.out.print(i + " ");
                 flag = true;
             }
         }
  
+        // check if Kaprekar number is flag or not
         if (!flag) {
             System.out.println("INVALID RANGE");
         } else {
