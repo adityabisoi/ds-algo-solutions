3519b3d662fcf69a97c31fb2d61aef8ebcbe09e1
abhay313 <abhayiiitk@gmail.com>
2020-12-19 17:16:34 +0530

    Cpp solution of hurdle race and designer pdf viewer added

---
+++ Algorithms/Implementation/Designer PDF Viewer/solution.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Implementation Part
+int designerPdfViewer(vector<int> h, string word) {
+    
+    int heighest = 0, area;
+    
+    // find index of each letter of word in vector h,
+    // get h[index],
+    // find heighest value of letter from h 
+    for(int i = 0; i < word.size(); i++){
+        int index = (int)(word[i]) - 97;
+        if(heighest  < h[index])
+            heighest = h[index];
+    }
+    
+    // calculate area
+    area = heighest * word.size();
+    return area;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split_string(h_temp_temp);
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
