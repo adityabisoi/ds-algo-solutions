6c95641bb8620d895634748fb5265488129c63d7
anushkasinghal11 <singhalanushka.1999@gmail.com>
2020-12-19 17:51:02 +0530

    fibonacci

---
+++ Algorithms/Fibonacci Modified/solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the fibonacciModified function below.
+    static BigInteger fibonacciModified(int t1, int t2, int n) {
+
+        BigInteger a = new BigInteger(Integer.toString(t1));
+        BigInteger b = new BigInteger(Integer.toString(t2));
+        BigInteger c = new BigInteger("0");
+        for (int i = 2; i < n; i++) {
+            c = a.add(b.multiply(b));
+            a = b;
+            b = c;
+
+        }
+        return c;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] t1T2n = scanner.nextLine().split(" ");
+
+        int t1 = Integer.parseInt(t1T2n[0]);
+
+        int t2 = Integer.parseInt(t1T2n[1]);
+
+        int n = Integer.parseInt(t1T2n[2]);
+
+        BigInteger result = fibonacciModified(t1, t2, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
