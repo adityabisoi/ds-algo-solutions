197fd2449255f58319c0d855aa6fbe0df31c7ddc
RISHAV KUMAR <57145318+hr1shu@users.noreply.github.com>
2020-12-19 22:55:39 +0530

    New algos

---
+++ Important algorithms/Smallest Subarray with sum X/solution.py
@@ -0,0 +1,32 @@
+def window(arr,n, k):
+    left=0
+    right=0
+    ans=n
+    sum1=0
+    while left<n and right<n+1:
+            if sum1>k:
+                if left==right:
+                    ans=1
+                    break
+                ans=min(ans,right-left)
+                sum1-=arr[left]
+                left+=1
+            elif right==n:
+                break
+            else:
+                
+                sum1+=arr[right]
+                right+=1
+    return ans
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n, k = map(int,input().split())
+        arr = list(map(int,input().split()))
+        print(window(arr,n,k))
+        
+if __name__ == "__main__":
+    main()
+        
+    
