b1df6664fe2a60dbc696704ff3fc996ffb19dde8
Devansh Shukla <46221293+Devansh252@users.noreply.github.com>
2020-12-20 17:04:07 +0530

    Solution for Modified Knapsack python

---
+++ Algorithms/Dynamic Programming/Fibonacci Modified/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+
+
+def fibonacciModified(t1, t2, n):  #function taking arguments for series
+    i = 2
+    while i < n:
+        t1, t2= t2 , t1 + t2*t2 # modifying values simyltaneously t1=t2 and t2 = t1+t2^2 
+        i += 1
+    return t2    # returning n th element
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')  
+
+    # Taking inputs
+    
+    t1T2n = raw_input().split()   
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n) # calling function
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
