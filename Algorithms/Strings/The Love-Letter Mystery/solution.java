e8e7ddb74a1c9f4aebcd25cfea752f84d8ea78cc
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-22 12:31:29 +0530

    comment

--- Algorithms/Strings/The Love-Letter Mystery/solution.java
+++ Algorithms/Strings/The Love-Letter Mystery/solution.java
@@ -8,11 +8,13 @@ import java.util.regex.*;
 
 public class Solution {
 
+    // theLoveLetterMystery function
     static int theLoveLetterMystery(String s) {
         int count = 0;
-        int i = 0; 
-        int j = s.length() - 1; 
+        int i = 0; // pointer at starting of string s
+        int j = s.length() - 1; // pointer at end of string s
 
+        // check for palindromic string
         while (i <= j) {
             if (s.charAt(i) != s.charAt(j)) {
                 count += Math.abs(((int) s.charAt(i)) - ((int) s.charAt(j)));
@@ -20,20 +22,22 @@ public class Solution {
             i++; j--;
         }
         
+        // minimum number of operations corresponding to each test case
         return count;
 
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    // main function
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int q = scanner.nextInt();
+        int q = scanner.nextInt(); // number of queries
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         for (int qItr = 0; qItr < q; qItr++) {
-            String s = scanner.nextLine();
+            String s = scanner.nextLine(); // string
 
             int result = theLoveLetterMystery(s);
 
