5f77f23cefc808d034a897ebf4f5e9a56c04788b
Aarushi Hans <67260600+Aarushi11H@users.noreply.github.com>
2020-12-22 15:47:31 +0530

    Update solution.java

--- Algorithms/Strings/Highest Value Palindrome/solution.java
+++ Algorithms/Strings/Highest Value Palindrome/solution.java
@@ -20,12 +20,20 @@ public class solution {
             return "-1";
         }
         int changeBoth = k - minChange;
+          
+    // Iinitialize l and r by leftmost and 
+    // rightmost ends 
         int i = 0;
         int j = chars.length - 1;
         for (; i <= j; i++, j--) {
             if (chars[i] != chars[j]) {
+                 // Replace left and right character by  
+            // maximum of both  
                 char maxChar = (char) Math.max(chars[i], chars[j]);
                 if (maxChar != '9' && changeBoth - 1 >= 0) {
+                     /* If characters at ith or rth (one of them) is changed in the previous  
+                loop then subtract 1 from K (1 more is  
+                subtracted already) and make them 9 */ 
                     chars[i] = '9';
                     chars[j] = '9';
                     changeBoth--;
@@ -36,6 +44,9 @@ public class solution {
                 }
             } else {
                 char maxChar = (char) Math.max(chars[i], chars[j]);
+                 /* If none of them is changed in the 
+               previous loop then subtract 2 from changeboth
+               and convert both to 9 */
                 if (maxChar != '9' && changeBoth - 2 >= 0) {
                     chars[i] = '9';
                     chars[j] = '9';
