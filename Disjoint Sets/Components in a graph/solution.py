c3009acb691c2d02703f0b27a935e68d3ad82fa5
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-12-23 10:21:24 +0530

    solution.py

--- Disjoint Sets/Components in a graph/solution.py
+++ Disjoint Sets/Components in a graph/solution.py
@@ -13,7 +13,9 @@ def find(i, root):
 
 def union(x, y, root, count):
     if x != y:
-        root[y] = x
+         # union without ranking
+        root[y] = x # // updating the parent/root of the new component
+     #updating the size of the new component
         count[x] += count[y]
         count[y] = 0
     return root, count
@@ -23,12 +25,14 @@ root = [i for i in range(2*n+1)]
 count = [1 for i in range(2*n+1)]
 for i in range(n):
     inp = list(map(int, input().split()))
+    # find root of a & b
     x = find(inp[0]-1, root)
     y = find(inp[1]-1, root)
     root, count = union(x, y, root, count)
 mincount = 2*n+1
 maxcount = 0
 for i in range(2*n+1):
+     # find min and max
     if count[i] > 1 and count[i] < mincount:
         mincount = count[i]
     if count[i] > maxcount:
