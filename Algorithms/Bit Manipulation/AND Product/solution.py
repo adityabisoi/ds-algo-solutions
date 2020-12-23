e2e9d350c1eb70d84650e6135adb44b2c5198d9e
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-12-23 23:42:59 +0530

    and product

--- Algorithms/Bit Manipulation/AND Product/solution.py
+++ Algorithms/Bit Manipulation/AND Product/solution.py
@@ -10,9 +10,11 @@ T = int(input())
 
 for _ in range(T) :
     A,B = (int(_) for _ in input().split())
-    i,C = -1,A^B
+    i,C = -1,A^B #C is the bits where A and B differ
     while C > 0 :
-        C >>= 1
-        i += 1
+        C >>= 1     # mask out all lower bits than the max bit of C
+                    
+        i += 1 #increase the value of i (2's power)
+     # remove those bits
     print(A & (2**32-2**i))
     
