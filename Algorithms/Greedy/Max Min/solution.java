8d9cdc569aaf717520bda5047ca35d647ac8c52b
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-25 15:00:05 +0530

    comments

--- Algorithms/Greedy/Max Min/solution.java
+++ Algorithms/Greedy/Max Min/solution.java
@@ -7,13 +7,14 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-    
+
+    // maxMin function
     static int maxMin(int k, int[] arr) {
-        Arrays.sort(arr);
+        Arrays.sort(arr); // Sorts the array in ascending order
         
         int min = Integer.MAX_VALUE;
         for(int i = 0; i <= arr.length - k; i++) 
-            min = Math.min(min, arr[k + i - 1] - arr[i]); 
+            min = Math.min(min, arr[k + i - 1] - arr[i]); // Keeps a running min of the beginning and end of subarrays
         return min;
     }
 
@@ -22,10 +23,10 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = scanner.nextInt();
+        int n = scanner.nextInt(); // number of elements in array
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int k = scanner.nextInt();
+        int k = scanner.nextInt(); // number of elements to select
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int[] arr = new int[n];
@@ -36,7 +37,7 @@ public class Solution {
             arr[i] = arrItem;
         }
 
-        int result = maxMin(k, arr);
+        int result = maxMin(k, arr); // calling maxMin function
  
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
@@ -46,3 +47,8 @@ public class Solution {
         scanner.close();
     }
 }
+
+/*
+ Time Complexity: O(n log(n))
+ Space Complexity: O(1)         
+*/
