9d4c7f347ed2a8750c05d298bff242c2f49e5856
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-25 21:57:39 +0530

    add comments

--- Algorithms/Greedy/Goodland Electricity/solution.py
+++ Algorithms/Greedy/Goodland Electricity/solution.py
@@ -1,7 +1,12 @@
+# input the number of cities in Goodland and the plants' range constant
 n,k = map(int, input().strip().split(' '))
+# input the array elements
 a = list(map(int, input().strip().split(' ')))
 i = k-1
 j = i
+#Print a single integer denoting the minimum number of plants 
+#that must be built so that all of Goodland's cities have electricity
+#If this is not possible for the given value of k, print -1
 while a[j]!=1 and j>-1:
     j -= 1
     if j==-1:
