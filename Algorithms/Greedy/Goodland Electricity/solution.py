69af46be7b4c4ee564a68e1854ae377a427c2ab4
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-25 21:55:04 +0530

    Create solution.py

---
+++ Algorithms/Greedy/Goodland Electricity/solution.py
@@ -0,0 +1,28 @@
+n,k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+i = k-1
+j = i
+while a[j]!=1 and j>-1:
+    j -= 1
+    if j==-1:
+        print('-1')
+        quit()
+rep = 1
+i = j+2*k-1
+while i<n:
+    j = i
+    while a[j]!=1 and j>i-(2*k-1): j -= 1
+    if j==i-(2*k-1):
+        print('-1')
+        quit()
+    i = j+2*k-1
+    rep += 1
+if j+k-1<n-1:
+    j = n-1
+    while a[j]!=1 and j>i-(2*k-1): j -= 1
+    if j==i-(2*k-1):
+        print('-1')
+        quit()
+    rep += 1
+print(rep)
+        
