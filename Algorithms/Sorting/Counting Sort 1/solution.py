1a0c697644cb2cde3a178c329693546d1286c68e
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-26 21:54:41 +0530

    comments

--- Algorithms/Sorting/Counting Sort 1/solution.py
+++ Algorithms/Sorting/Counting Sort 1/solution.py
@@ -1,5 +1,6 @@
+#input number of items in array
 N = int(input())
-
+#input array elements
 arr = list(map(int, input().split()))
 
 freq = [0]*100
@@ -7,5 +8,5 @@ freq = [0]*100
 for i in arr:
     freq[i] = freq[i]+1
 
-
+#output the sorted array
 print (' '.join(map(str, freq)))
