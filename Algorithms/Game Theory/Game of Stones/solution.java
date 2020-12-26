dce786ba2d2a68c172035c6dca84fc7427eaa22b
anushkasinghal11 <singhalanushka.1999@gmail.com>
2020-12-26 22:52:12 +0530

    update solution

---
+++ Algorithms/Game Theory/Game of Stones/solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the gameOfStones function below.
+    static String gameOfStones(int n) {
+        if (n % 7 <= 1)
+            return "Second";
+        else
+            return "First";
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String result = gameOfStones(n);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
