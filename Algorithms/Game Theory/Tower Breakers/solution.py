50fedf89f72859f899c802408a458d1605f12bbd
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-26 22:13:46 +0530

    add comments

--- Algorithms/Game Theory/Tower Breakers/solution.py
+++ Algorithms/Game Theory/Tower Breakers/solution.py
@@ -1,6 +1,9 @@
+#input number of test cases
 test = int(input())
 for _ in range(test):
+    #input number of towers and height of towers
     n,m = map(int,input().strip().split())
+    #if the first player wins, print 1, otherwise, print 2
     if m==1:
         print(2)
     else:
