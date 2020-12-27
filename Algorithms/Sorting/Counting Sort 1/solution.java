04c0eab88f40a042a57cd0c889f358da15f53ef8
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-27 13:44:42 +0530

    comments counting sort 1

--- Algorithms/Sorting/Counting Sort 1/solution.java
+++ Algorithms/Sorting/Counting Sort 1/solution.java
@@ -5,17 +5,20 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // the number of items in array
         int[] frequencies = new int[100];
+        // array elements
         for(int i = 0; i < n; i++)
         {
             int num = sc.nextInt();
             frequencies[num] = frequencies[num] + 1;
         }
-         
+        
+        // number of times every number from 0 through 99 appears in array 
         for(int i = 0; i < frequencies.length; i++)
         {
             System.out.print(frequencies[i]+" ");
         }
     }
 }
+
