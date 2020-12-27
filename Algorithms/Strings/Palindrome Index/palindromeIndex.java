7e5bf3b8727b62cecdde9181fa75363d26bc335d
khushi  balyan <52583098+khushibalyan220101@users.noreply.github.com>
2020-12-27 21:13:14 +0530

    Create palindromeIndex.java

---
+++ Algorithms/Strings/Palindrome Index/palindromeIndex.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the palindromeIndex function below.
+    static int palindromeIndex(String s) {
+        int l = s.length();
+        int i,j,a,b;
+        for (i=0, j=l-1; i<l; i++,j--){
+            if (s.charAt(i)!=s.charAt(j))
+                break;
+        }
+        if (i>j) return -1;
+
+        for (a = i+1, b = j;a<j && b>i+1; a++,b--){
+            if (s.charAt(a)!=s.charAt(b))
+                return j;
+        }
+        return i;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = palindromeIndex(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
