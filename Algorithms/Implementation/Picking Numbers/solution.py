73825e8c6458e6c297b1093706336d8f259094fe
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-28 20:38:51 +0530

    Update solution.py

---
+++ Algorithms/Implementation/Picking Numbers/solution.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+d = {x:0 for x in range(1,100)}
+
+for i in a:
+    d[i] = d.get(i,0)+1
+
+maxi = 0
+arr = list(d.keys())
+for i in range(1,99):
+    maxi = max(maxi,d[i]+d[i+1])
+print(maxi)
+    
