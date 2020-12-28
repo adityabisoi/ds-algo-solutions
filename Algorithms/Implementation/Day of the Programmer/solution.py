a2ba58d6763c327fb4255fa3b4acccc672220936
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-28 20:36:40 +0530

    comments

--- Algorithms/Implementation/Day of the Programmer/solution.py
+++ Algorithms/Implementation/Day of the Programmer/solution.py
@@ -6,4 +6,5 @@ elif y > 1918:
     ftext = "13.09.{}" if (y & 3) or (y % 400 and not y % 100) else "12.09.{}"
 else:
     ftext = "26.09.{}"
+# Print the full date of Day of the Programmer during year  in the format dd.mm.yyyy
 print(ftext.format(y))
