bfc91c9fc4aa7b808d5ebb4b9eacf90194fe007a
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-28 22:15:35 +0530

    comments

--- Algorithms/Implementation/Circular Array Rotation/solution.java
+++ Algorithms/Implementation/Circular Array Rotation/solution.java
@@ -8,22 +8,27 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt()%n;
-        int q = sc.nextInt();
+        int n = sc.nextInt(); // number of elements in the integer array
+        int k = sc.nextInt()%n;// because every n rotations we receive the same array
+        // k is rotation count
+        int q = sc.nextInt(); // number of queries
         int[] a = new int[n];
         for(int i=0; i < n; i++){
             a[i] = sc.nextInt();
         }
-
+        
+        
+        //Run each query q
         for(int i = 0; i < q; i++){
-            int m = sc.nextInt();
+            int m = sc.nextInt(); // index of an element in array to return
+            //Checks if array wrap occurs
             if(m-k >=0)
             {
                 System.out.println(a[m-k]);
             }
             else
             {
+                //if array rap occurs then
                 System.out.println(a[n+(m-k)]);
             }
             
