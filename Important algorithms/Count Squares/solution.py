288bc7cf7c6297e71aab2101fd00e7e700840b00
RISHAV KUMAR <57145318+hr1shu@users.noreply.github.com>
2020-12-29 09:43:26 +0530

    New algos

---
+++ Important algorithms/Count Squares/solution.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def countSquares(self, N):
+        # code here
+        a = int(N**(0.5))
+        if a**2 == N:
+            return a-1
+        return a
+        
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import math
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.countSquares(N))
+# } Driver Code Ends
---
+++ Important algorithms/First and last occurence of element/solution.py
@@ -0,0 +1,13 @@
+t=int(input())
+for _ in range(t):
+    (n,s)=map(int,input().split())
+    a=list(map(int,input().split()))
+    c=[]
+    for i in range(n):
+        if a[i]==s:
+            c.append(i)
+    if len(c)>0:
+        x=len(c)
+        print(c[0],c[x-1])
+    else:
+        print(-1)
