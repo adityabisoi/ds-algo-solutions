a149f44cd304550f6fa6069ef8d8ee2803f84dc7
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-29 22:15:57 +0530

    function

--- Algorithms/Recursion/The Power Sum/solution.py
+++ Algorithms/Recursion/The Power Sum/solution.py
@@ -3,3 +3,17 @@ import sys
 x = int(input().strip())
 n = int(input().strip())
 
+def fun(a,min):
+    if a == 0:
+        return 1
+    if min**n > a:
+        return 0
+
+    count = 0
+    current = min
+    while(a-current**n >= 0):
+        count += fun(a-current**n,current + 1)
+        current += 1
+    return count
+
+print(fun(x,1))
