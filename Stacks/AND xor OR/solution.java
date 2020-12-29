093e53d3ac151683afbb5adefb6b7fcdf0bf6d85
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-12-29 22:31:05 +0530

    AND xor OR comments added

--- Stacks/AND xor OR/solution.java
+++ Stacks/AND xor OR/solution.java
@@ -7,15 +7,15 @@ public class Solution {
         
                 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // size of array
 
-        int[] a = new int[n];
+        int[] a = new int[n]; // integer array
         for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
         
         
-        Stk stk1 = new Stk(n);
+        Stk stk1 = new Stk(n); // object creation
         
         int s = 0;
         
@@ -25,11 +25,11 @@ public class Solution {
         min = a[0];
         stk1.push(0);
         
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n;i++){ // iterating
             
             if(a[stk1.peep()]<a[i])
             {
-                int m = sum(a[stk1.peep()], a[i]);
+                int m = sum(a[stk1.peep()], a[i]); // sum function call
                 if(m>max)
                     max=m;
                 
@@ -38,14 +38,14 @@ public class Solution {
             else if(a[stk1.peep()]>a[i]){
                 
                 while(stk1.peep()!=-99 && a[stk1.peep()]>a[i]){
-                    int m = sum(a[stk1.pop()], a[i]);
+                    int m = sum(a[stk1.pop()], a[i]); // sum function call
                     if(m>max)
                         max=m;
                 }
                 
                 if(stk1.peep()!=-99){
                     int k = a[stk1.peep()];
-                    int m = sum(k, a[i]);
+                    int m = sum(k, a[i]); // sum function call
                     if(m>max)
                         max=m;
                 }
@@ -58,12 +58,14 @@ public class Solution {
         
     }
     
+    // sum function return value after operation
     public static int sum(int a , int b)
     {
         return (((a&b)^(a|b))&(a^b));
     }
 }
 
+// Stk class
 class Stk {
 
     private int[] stkArr = null;
@@ -78,12 +80,13 @@ class Stk {
 
     private int total = 0;
 
-    public Stk(int size) { 
+    public Stk(int size) { // Stk constructor 
         super();
         this.size = size;
         stkArr = new int[size];
     }
 
+    // push function to insert element
     public void push(int x) {
         currentPointer++;
         stkArr[currentPointer] = x;
@@ -92,10 +95,12 @@ class Stk {
 
     }
 
+    // getTotal function for getting total
     public int getTotal() {
         return total;
     }
 
+    // pop function to remove element
     public int pop() {
         int val = stkArr[currentPointer];
         stkArr[currentPointer] = 0;
@@ -107,6 +112,7 @@ class Stk {
 
     }
 
+    // peep function to return peek
     public int peep() {
         if (currentPointer > -1)
             return stkArr[currentPointer];
@@ -114,14 +120,17 @@ class Stk {
             return -99;
     }
 
+    // getCurrentPointer function to get Current Pointer
     public int getCurrentPointer() {
         return currentPointer;
     }
 
+    // getSize function to get size 
     public int getSize() {
         return size;
     }
 
+    // getMax function to get maximum
     public int getMax() {
         return stkMaxArr[maxPointer];
     }
