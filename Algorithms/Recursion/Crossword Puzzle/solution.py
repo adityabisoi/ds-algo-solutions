dd63688bb93ccd71db275a369c7e77e97ed269c9
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-29 22:11:18 +0530

    Update solution.py

--- Algorithms/Recursion/Crossword Puzzle/solution.py
+++ Algorithms/Recursion/Crossword Puzzle/solution.py
@@ -10,6 +10,16 @@ for lst in (horizontal, vertical):
         lst.append([row, l[0], l[-1]])
   A = list(map(list, zip(*A)))
 
-  intersections = [(i, column[0] - row[1], j, row[0] - column[1]) \
+intersections = [(i, column[0] - row[1], j, row[0] - column[1]) \
                  for i, row in enumerate(horizontal) for j, column in enumerate(vertical) \
                  if row[1] <= column[0] <= row[2] and column[1] <= row[0] <= column[2]]
+
+words = input().split(';')
+result = None
+for l in itertools.permutations(words, len(words)):
+  if all(len(l[i]) == horizontal[i][2] - horizontal[i][1] + 1 for i in range(len(horizontal))) and \
+     all(len(l[i + len(horizontal)]) == vertical[i][2] - vertical[i][1] + 1 for i in range(len(vertical))) and \
+     all(l[i[0]][i[1]] == l[i[2] + len(horizontal)][i[3]] for i in intersections):
+    result = l
+    break
+
