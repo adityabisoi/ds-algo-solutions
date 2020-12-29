603284e7bab8772b338c145b8952e6448c9b6d01
TheWire <psatyam360@gmail.com>
2020-12-30 01:09:37 +0530

    Readme Updated

--- Algorithms/Implementation/Divisible Sum Pairs/solution.cpp
+++ Algorithms/Implementation/Divisible Sum Pairs/solution.cpp
@@ -6,7 +6,7 @@
     Traverse the array make possible pair of i & j such that j is always greater than i
 */
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -15,19 +15,19 @@ vector<string> split_string(string);
 // Complete the divisibleSumPairs function below.
 int divisibleSumPairs(int n, int k, vector<int> ar) {
     int count=0; // to store count of such pairs
-        //Traverse through the array
-        for(int i=0;i<n; i++){
-            int j=i+1;
-            // make sure j is greater than i
-            while(j<n){
-                // if sum of elements present at index at i & j is divisible by k increase the count
-                if((ar[i]+ar[j])%k==0){
-                    count++;
-                }
-                j++;
+    //Traverse through the array
+    for(int i=0;i<n; i++){
+        int j=i+1;
+        // make sure j is greater than i
+        while(j<n){
+            // if sum of elements present at index at i & j is divisible by k increase the count
+            if((ar[i]+ar[j])%k==0){
+                count++;
             }
+            j++;
         }
-        return count; // return the answer
+    }
+    return count; // return the answer
 
 }
 
