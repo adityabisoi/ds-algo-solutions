ee5fcf5c164ae2b8f3c788d858807377a4b95214
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-12-29 13:17:31 +0530

    solution

--- Algorithms/Greedy/Reverse Shuffle Merge/solution.java
+++ Algorithms/Greedy/Reverse Shuffle Merge/solution.java
@@ -36,8 +36,19 @@ public class Solution {
         for (int i = 0; i < s.length(); i++) {
             cd[s.charAt(i)-'a'].total++;
         }
+        // Character counts we need in the shuffled 
+        // version of our original string
         char [] r = new char[s.length()/2];
         int ri=0;
+          /*  See if this character should appear before any 
+    previous ones. Basically, if this char is smaller 
+    than the previous char, and the previous char 
+    still occurs in the chars of the shuffled string, 
+    we can the safely replace the previous char 
+    with this one. That's so because we should be 
+    able to still create a valid string which contains
+    both characters although the order will differs.
+*/
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             CharData di = cd[ch-'a'];
@@ -51,7 +62,7 @@ public class Solution {
                 di.taken++;
             }
         }
-        System.out.println(new String(r));
+        System.out.println(new String(r)); // // return string which is the lexicographically smallest valid string r
         in.close();
     }
 }
