a126c98173814c3922444707772e00d3ff213a1f
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-30 18:14:42 +0530

    Update solution.py

--- Stacks/Poisonous Plants/solution.py
+++ Stacks/Poisonous Plants/solution.py
@@ -1,2 +1,10 @@
 def solve(ar):
     stack, max_days = [], 0
+    for x in ar:
+        days = 0
+        while stack and x <= stack[-1][0]:
+            days = max(days, stack.pop()[1])
+        days = days+1 if stack else 0
+        max_days = max(days, max_days)
+        stack.append([x, days])
+    print(max_days)
