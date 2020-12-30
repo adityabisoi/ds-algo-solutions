6be426421887441e951534b080316ee54511057e
anushkasinghal11 <singhalanushka.1999@gmail.com>
2020-12-30 22:01:29 +0530

    classname

--- Algorithms/Dynamic Programming/Red John is Back/solution.java
+++ Algorithms/Dynamic Programming/Red John is Back/solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class solution {
 
     // Complete the redJohn function below.
     static int redJohn(int n) {
