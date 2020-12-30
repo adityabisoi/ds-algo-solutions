071c9aeed7af3fb14c047822f93ea22f4e26cc62
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-30 18:18:27 +0530

    Update solution.py

--- Stacks/AND xor OR/solution.py
+++ Stacks/AND xor OR/solution.py
@@ -4,3 +4,17 @@ ints = [int(x) for x in input().split()]
 stack = []
 stack.append(ints[0])
 max = -1
+
+for i in range(1, size):
+    while(len(stack) > 0 and ints[i] <= stack[len(stack)-1]):
+        top = stack.pop()
+        s = top ^ ints[i]
+        if s > max:
+            max = s
+    if len(stack) > 0 and ints[i] > stack[len(stack)-1]:
+        top = stack[len(stack)-1]
+        s = top ^ ints[i]
+        if s > max:
+            max = s
+    stack.append(ints[i])
+print(max)
