049d761eeb75c14c1f6b9ad58a6e0dde61056c6d
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2020-12-31 23:57:56 +0530

    Update solution.py

--- Algorithms/Dynamic Programming/Stock Maximize/solution.py
+++ Algorithms/Dynamic Programming/Stock Maximize/solution.py
@@ -4,3 +4,13 @@ for i in range(test):
     a=list(map(int,input().split()))
     c=0
     i=len(a)-1
+    while(i>=0):
+        d=a[i]
+        l=i
+        p=0
+        while(a[i]<=d and i>=0):
+            p+=a[i]
+            i-=1
+        c+=(l-i)*a[l]-p
+        continue
+    print (c)
