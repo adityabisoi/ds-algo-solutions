0fe98a52123b64eaf32d7420da8719d6974486f5
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-01 11:25:25 +0530

    update

--- Algorithms/Game Theory/Nimble Game/solution.java
+++ Algorithms/Game Theory/Nimble Game/solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class solution {
 
     // Complete the nimbleGame function below.
     static String nimbleGame(int[] s) {
