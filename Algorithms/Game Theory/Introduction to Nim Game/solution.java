11f3bbb7362ea2c8d0757e9a89d3145da74aa4af
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-01 12:19:31 +0530

    classname

--- Algorithms/Game Theory/Introduction to Nim Game/solution.java
+++ Algorithms/Game Theory/Introduction to Nim Game/solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class solution {
 
     // Complete the nimGame function below.
     static String nimGame(int[] pile) {
