798d5c0311aa3bcdb90c5da1e1f1f853f6fc9c73
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-01 21:42:44 +0530

    Update solution.py

--- Algorithms/Game Theory/Misère Nim/solution.py
+++ Algorithms/Game Theory/Misère Nim/solution.py
@@ -3,3 +3,15 @@ for _ in range(test):
     pile = int(input())
     ar = list(map(int,input().strip().split()))
     xor = 0
+    for n in ar:
+        xor = xor^n
+    if ar == [1]*len(ar):
+        if len(ar)%2 == 0:
+            print('First')
+        else:
+            print('Second')
+    else:
+        if xor==0:
+            print('Second')
+        else:
+            print('First')
