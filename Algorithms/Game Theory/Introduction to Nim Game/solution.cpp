72c79c5a0c1e00831808d29e4b9d2249c44cec25
TheWire <psatyam360@gmail.com>
2021-01-01 20:21:59 +0530

    Nimble game solution in C++

--- Algorithms/Game Theory/Introduction to Nim Game/solution.cpp
+++ Algorithms/Game Theory/Introduction to Nim Game/solution.cpp
@@ -1,5 +1,11 @@
 /*
-    
+    This is a standard Nim game where you need to find the exclusive OR (XOR) of all stone piles to
+     determine the winning and losing positions.
+      To understand how it works, we recommend you to read 
+      - https://www.geeksforgeeks.org/combinatorial-game-theory-set-2-game-nim/?ref=rp
+
+    The quickest solution is to XOR all values of array arr together. 
+    If the value is 1, player 1 will win. Otherwise, it will be player 2.
 */
 #include <bits/stdc++.h>
 
