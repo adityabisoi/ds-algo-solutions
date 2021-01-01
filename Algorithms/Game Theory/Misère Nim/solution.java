0af0b4e5d08991f1e00063c5193c6a58badead55
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-01 12:15:41 +0530

    correction

--- Algorithms/Game Theory/Misère Nim/solution.java
+++ Algorithms/Game Theory/Misère Nim/solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class solution {
 
     // Complete the misereNim function below.
     static String misereNim(int[] s) {
