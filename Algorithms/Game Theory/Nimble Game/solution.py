27faf7a1573abac21580fe1c141bfbdc2fba00bc
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-01 21:46:59 +0530

    Update solution.py

--- Algorithms/Game Theory/Nimble Game/solution.py
+++ Algorithms/Game Theory/Nimble Game/solution.py
@@ -2,3 +2,6 @@ test = int(input())
 for _ in range(test):
     n = int(input())
     ar = list(map(int,input().strip().split()))
+    result = []
+    for i in range(len(ar)):
+        result.append(ar[i]%2)
