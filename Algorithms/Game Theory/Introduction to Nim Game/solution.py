4d45aa9f406cefadd7fb2f421c0c76f18113d0a7
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-01 21:38:26 +0530

    Update solution.py

--- Algorithms/Game Theory/Introduction to Nim Game/solution.py
+++ Algorithms/Game Theory/Introduction to Nim Game/solution.py
@@ -2,3 +2,10 @@ test = int(input())
 for _ in range(test):
     pile = int(input())
     ar = list(map(int,input().strip().split()))
+    xor = 0
+    for n in ar:
+        xor = xor^n
+    if xor == 0:
+        print('Second')
+    else:
+        print('First')
