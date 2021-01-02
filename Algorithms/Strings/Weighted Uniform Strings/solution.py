5542d2d731665e2723e17f065cf29b70c4e4c93e
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-02 17:43:23 +0530

    Update solution.py

--- Algorithms/Strings/Weighted Uniform Strings/solution.py
+++ Algorithms/Strings/Weighted Uniform Strings/solution.py
@@ -2,3 +2,9 @@ s = input().strip()
 cost = set()
 prev = ''
 count = 0
+for i in s:
+    if i != prev:
+        prev = i
+        count = 0
+    count += 1
+    cost.add(count * (ord(i) - ord('a') + 1))
