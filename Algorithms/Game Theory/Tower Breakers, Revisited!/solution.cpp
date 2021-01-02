3d89ef701fc503df4fdaee54c71a45ef38807252
TheWire <psatyam360@gmail.com>
2021-01-02 16:54:19 +0530

    Tower breakers solution in C++

--- Algorithms/Game Theory/Tower Breakers, Revisited!/solution.cpp
+++ Algorithms/Game Theory/Tower Breakers, Revisited!/solution.cpp
@@ -1,9 +1,9 @@
 /*
-    Imagine each tower as a Nim pile which has a Nimvalue equal to the number of prime factors of .
+    Imagine each tower as a Nim pile which has a Nimvalue equal to the number of prime factors of hi.
      Reducing a tower to its divisor is the same as taking away a non-zero prime factor from it.
 
     Thus, this game is the same as a Nim game and our answer is the XOR of all Nim piles. 
-    If the Nim sum is , then player  wins; otherwise, player  wins.
+    If the Nim sum is 0, then player  2wins; otherwise, player 1 wins.
 */
 
 #include <cmath>
