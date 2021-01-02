2bd4a27b8a2a06ba3d9579cb2efc175f3f23b045
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-02 17:37:46 +0530

    Update solution.py

--- Algorithms/Strings/Two Characters/solution.py
+++ Algorithms/Strings/Two Characters/solution.py
@@ -6,4 +6,11 @@ for i in set(s):
         if(j!=i):
             temp=[k for k in s if k==j or k==i]
             f=0
-            
+            for z in range(1,len(temp)):
+                if(temp[z]==temp[z-1]):
+                    f=1
+                    break                    
+            if(f==0):          
+                if(len(temp)>ans):
+                    ans=len(temp)
+print(ans)
