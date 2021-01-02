0f87d0423ae91b5c52e5e62fd98164f53126dc10
abhay313 <abhayiiitk@gmail.com>
2021-01-02 09:09:13 +0530

    added comments

--- Algorithms/Strings/Weighted Uniform Strings/solution.cpp
+++ Algorithms/Strings/Weighted Uniform Strings/solution.cpp
@@ -2,36 +2,39 @@
 
 using namespace std;
 
-// Complete the weightedUniformStrings function below.
+// Implementation Part
 vector<string> weightedUniformStrings(string s, vector<int> queries) {
     
     vector<string> result;
     vector<int> u;
     int count = 1, prev = 0;
     
+    // get all possible values for u
     for(int i = 0; i < s.size(); i++){
         int weight = s[i] - 'a' + 1;
+        // substring for repeated characters
         if(weight == prev)
             count++;
         else{
             prev = weight;
             count = 1;
         }
-        
+        // calculate and store values
         u.push_back(weight * count);
     }
     
+    // check for each query
     for(int i = 0; i < queries.size(); i++){
         int val = queries[i];
+        // if query value found in vector u
         auto pos = find(u.begin(), u.end(), val);
         if(pos != u.end())
         result.push_back("Yes");
+        // if query value not found
         else {
         result.push_back("No");
         }
     }
-    
-
     return result;
 }
 
