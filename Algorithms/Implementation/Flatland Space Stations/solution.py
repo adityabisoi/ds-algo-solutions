d7b3e7cb6e033cc34e625cc91eb81656788d1076
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-03 21:19:45 +0530

    Update solution.py

--- Algorithms/Implementation/Flatland Space Stations/solution.py
+++ Algorithms/Implementation/Flatland Space Stations/solution.py
@@ -5,3 +5,18 @@ def main():
     l = [10 ** 5] * n
     for x in c:
         l[x] = 0
+    
+    for i in range(1, len(l)):
+        if l[i] == 0:
+            continue
+        else:
+            l[i] = min(l[i], l[i - 1] + 1)
+
+    for i in range(len(l) - 2, -1, -1):
+        if l[i] == 0:
+            continue
+        else:
+            l[i] = min(l[i], l[i + 1] + 1)
+
+    print(max(l))
+
