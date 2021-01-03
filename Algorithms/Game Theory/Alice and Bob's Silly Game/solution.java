27ee112f7b6833e8836bee9a7916e9b1c0bd8e02
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2021-01-03 21:08:58 +0530

    comments added

--- Algorithms/Game Theory/Alice and Bob's Silly Game/solution.java
+++ Algorithms/Game Theory/Alice and Bob's Silly Game/solution.java
@@ -6,25 +6,27 @@ import java.util.regex.*;
 
 public class Solution {
 
+    // sillyGame function
     static String sillyGame(int n) {
         int tmp = 0;
-        boolean[] arr = new boolean[n+1];
+        boolean[] arr = new boolean[n+1]; // boolean array of length n+1
         for (int x = 2; x <= n; x++){
-            if (arr[x] == false){ 
+            if (arr[x] == false){ // if element of array is false
                 tmp++;
                 for (int i = x; i <= n; i += x){
                     arr[i] = true;
                     }
                 }
             }
-        if(tmp % 2 == 0) 
+        if(tmp % 2 == 0) // if even
             return "Bob";
-        else        
+        else                // if odd 
             return "Alice";
     }
 
     private static final Scanner scanner = new Scanner(System.in);
     
+    // main function
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
