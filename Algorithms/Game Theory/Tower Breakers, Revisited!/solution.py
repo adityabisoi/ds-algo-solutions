28484be3fab9651e58f95272ef776d9ac91ada5c
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-03 07:37:31 +0530

    Update solution.py

--- Algorithms/Game Theory/Tower Breakers, Revisited!/solution.py
+++ Algorithms/Game Theory/Tower Breakers, Revisited!/solution.py
@@ -15,3 +15,23 @@ while(t != 0):
     a= [int(i) for i in input().split()]
     
     b = [0 for i in range(n)]
+    for i in range(n):
+        if(a[i] == 1):
+            b[i] = 0
+        else:
+            b[i] = 0
+            
+            j = 2
+            
+            root = int(pow(a[i],0.5))
+            while(a[i] != 1 and j<=root):
+                if(a[i]%j == 0):
+                    while(a[i]%j == 0):
+                        b[i] += 1
+                        a[i] = a[i]//j
+                    
+                j += 1
+                
+            if(a[i] != 1):
+                b[i] += 1
+            
