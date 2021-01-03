dbabcf878ecc1e9c0813acfe368363716183763b
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-03 07:47:13 +0530

    Update solution.py

--- Algorithms/Game Theory/Poker Nim/solution.py
+++ Algorithms/Game Theory/Poker Nim/solution.py
@@ -2,3 +2,9 @@ for t in range(int(input())):
     n = [int(x) for x in input().split()]
     a = [int(x) for x in input().split()]
     s = 0
+    for i in a:
+        s ^= i
+    if s == 0:
+        print("Second")
+    else:
+        print("First")
