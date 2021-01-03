94972d9bfa4566a174ce2aef1dca989339964a6b
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-03 07:31:13 +0530

    Update solution.py

--- Algorithms/Game Theory/Alice and Bob's Silly Game/solution.py
+++ Algorithms/Game Theory/Alice and Bob's Silly Game/solution.py
@@ -1,1 +1,12 @@
 import sys
+
+prime_pre = [0 for i in range(100001)]
+sieve = [0 for i in range(100001)]
+for i in range(2, 100000):
+    if sieve[i] == 0:
+        prime_pre[i] = prime_pre[i-1]+1
+        #primes.append(i)
+        for j in range(i, 100001, i):
+            sieve[j] = i
+    else:
+        prime_pre[i] = prime_pre[i-1]
