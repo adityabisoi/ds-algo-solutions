c9bd5d5215f33c1a24c2cec8fb978f209812ee51
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 17:46:39 +0530

    Update solution.py

--- Algorithms/Implementation/Almost Sorted/solution.py
+++ Algorithms/Implementation/Almost Sorted/solution.py
@@ -2,3 +2,13 @@ input()
 array = [int(x) for x in input().split(" ")]
 
 sorted_array = sorted(array)
+
+if array == sorted_array:
+    print("yes")
+else:    
+    wrong = []
+    for i, (a, b) in enumerate(zip(array, sorted_array)):
+        if a != b:
+            wrong.append(i)
+        if len(wrong) > 2:
+            break
