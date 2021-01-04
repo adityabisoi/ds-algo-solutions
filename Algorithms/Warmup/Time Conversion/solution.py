576c76972b605f08b5ea6784da4360495b65d38e
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 18:06:53 +0530

    comments

--- Algorithms/Warmup/Time Conversion/solution.py
+++ Algorithms/Warmup/Time Conversion/solution.py
@@ -1,3 +1,4 @@
+# A single string that represents a time in 12-hour clock format 
 ins = input().strip()
 
 is_pm = ins[-2:].lower() == 'pm'
@@ -8,5 +9,5 @@ if is_pm and time_list[0] < 12:
 
 if not is_pm and time_list[0] == 12:
     time_list[0] = 0
-
+# print a new string representing the input time in 24 hour format
 print(':'.join(map(lambda x: str(x).rjust(2, '0'), time_list)))
