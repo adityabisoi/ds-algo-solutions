4de5f21fe02709f5b5b69f9cb5e4411ea014df07
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 18:05:09 +0530

    Update solution.py

--- Algorithms/Warmup/Time Conversion/solution.py
+++ Algorithms/Warmup/Time Conversion/solution.py
@@ -2,4 +2,11 @@ ins = input().strip()
 
 is_pm = ins[-2:].lower() == 'pm'
 time_list = list(map(int, ins[:-2].split(':')))
- 
+
+if is_pm and time_list[0] < 12:
+    time_list[0] += 12
+
+if not is_pm and time_list[0] == 12:
+    time_list[0] = 0
+
+print(':'.join(map(lambda x: str(x).rjust(2, '0'), time_list)))
