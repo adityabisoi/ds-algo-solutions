d5fbb2718e4c87ec1119375ed6c94b8a93657229
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2021-01-04 12:58:21 +0530

    plus minus

---
+++ Algorithms/Warmup/Plus Minus/solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        
+        double positives = 0;
+        double negatives = 0;
+        double zeroes    = 0;
+        for (int i = 0; i < size; i++) {
+            int value = scan.nextInt();
+            if (value > 0) {
+                positives++;
+            } else if (value < 0) {
+                negatives++;
+            } else {
+                zeroes++;
+            }
+        }
+        scan.close();
+        
+        System.out.println(positives / size);
+        System.out.println(negatives / size);
+        System.out.println(zeroes / size);
+    }
+}
