171885e5c4be0449bcbc9887853f35e570a341db
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 17:41:25 +0530

    Update solution.py

--- Algorithms/Warmup/Plus Minus/solution.py
+++ Algorithms/Warmup/Plus Minus/solution.py
@@ -3,3 +3,11 @@ lists=[int(_) for _ in input().split()]
 p=0
 m=0
 z=0
+for ele in lists:
+    if(ele>0):
+        p+=1
+    else:
+        if(ele<0):
+            m+=1
+        else:
+            z+=1
