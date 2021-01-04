48f19b310fdcf710ba8e02ad675ff481a08cfe38
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2021-01-04 12:17:03 +0530

    simple array sum

---
+++ Algorithms/Warmup/Simple Array Sum/solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int simpleArraySum(int[] ar) {
+        int sum = 0;
+
+        for(int count = 0; count < ar.length; count++) {
+            sum = sum + ar[count];
+        }
+
+        return sum;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(scanner.nextLine().trim());
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+
+        for (int arItr = 0; arItr < arCount; arItr++) {
+            int arItem = Integer.parseInt(arItems[arItr].trim());
+            ar[arItr] = arItem;
+        }
+
+        int result = simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+    }
+}
