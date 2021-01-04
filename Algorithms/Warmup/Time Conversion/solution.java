3b647a05b49cd8a9a70a40093b8742e6db844961
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2021-01-04 13:13:46 +0530

    pm

--- Algorithms/Warmup/Time Conversion/solution.java
+++ Algorithms/Warmup/Time Conversion/solution.java
@@ -11,7 +11,7 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String time = sc.next(); 
         int hour = Integer.parseInt(time.substring(0,2))%12; // getting hours
-        if(time.contains("PM")) // if time contains "pm"
+        if(time.contains("PM")) // if time contains "PM"
             hour += 12;
         // printing time in 24 hour format
         System.out.print("%02d%s",hour,time.substring(2,8));
