29b4170dd3fb2add5f8a3217cdb325b46b6b9dae
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 17:50:43 +0530

    comments

--- Algorithms/Implementation/Almost Sorted/solution.py
+++ Algorithms/Implementation/Almost Sorted/solution.py
@@ -4,9 +4,11 @@ input()
 array = [int(x) for x in input().split(" ")]
 # store the sorted array
 sorted_array = sorted(array)
-
+# If the array is already sorted, output yes on the first line
 if array == sorted_array:
     print("yes")
+# If you can sort this array using one single operation out of the two permitted operations
+# then output yes on the first line and then do as given in the instructions
 else:    
     wrong = []
     for i, (a, b) in enumerate(zip(array, sorted_array)):
@@ -22,5 +24,6 @@ else:
         if sorted_array[wrong[0]:index] == list(reversed(array[wrong[0]:index])):
             print("yes")
             print("reverse", wrong[0]+1, index)
+        # If you cannot sort the array either way print no on the first line
         else:
             print("no")
