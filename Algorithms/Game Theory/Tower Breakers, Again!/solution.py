14cf3d6d4f4b956c1fabe997d288d5cb4bafdaf2
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 07:14:52 +0530

    Update solution.py

--- Algorithms/Game Theory/Tower Breakers, Again!/solution.py
+++ Algorithms/Game Theory/Tower Breakers, Again!/solution.py
@@ -3,3 +3,16 @@ for N in range(1, 10**5 + 1):
     i = N
     twos = 0
     c=0
+    while N%2==0:
+        N//=2
+        twos = 1
+    d = 3
+    while d*d <= N:
+        while N%d==0:
+            c += 1
+            N//=d
+        d += 2
+    if N>1:
+        c += 1
+    g.append(c+twos)
+    
