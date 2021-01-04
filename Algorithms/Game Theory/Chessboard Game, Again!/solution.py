d9d6c26d4b5dd4d7c494a83fe801757326d3ac3f
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 07:27:30 +0530

    Update solution.py

--- Algorithms/Game Theory/Chessboard Game, Again!/solution.py
+++ Algorithms/Game Theory/Chessboard Game, Again!/solution.py
@@ -1,1 +1,11 @@
 grun = {}
+def grundy(x, y):
+    if 0 < x < 16 > y > 0:
+        if (x, y) not in grun:
+            a = {grundy(x - 2, y - 1), grundy(x - 2, y + 1), grundy(x - 1, y - 2), grundy(x + 1, y - 2)}
+            k = 0
+            while k in a:
+                k += 1
+            grun[x, y] = k
+        return grun[x, y]
+    return -1
