cd23aa61dd7dcb1ae2161dae06cbff9a9f1eed3f
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-04 19:22:49 +0530

    classname

--- Algorithms/Warmup/Plus Minus/solution.java
+++ Algorithms/Warmup/Plus Minus/solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class solution {
 
     // main function
     public static void main(String[] args) {
