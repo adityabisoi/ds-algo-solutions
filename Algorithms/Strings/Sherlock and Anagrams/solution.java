0fc15d950c9fcffe3c58c55d538f71d732b7e72b
anushkasinghal11 <singhalanushka.1999@gmail.com>
2021-01-04 19:12:28 +0530

    comments

--- Algorithms/Strings/Sherlock and Anagrams/solution.java
+++ Algorithms/Strings/Sherlock and Anagrams/solution.java
@@ -6,7 +6,36 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+public class Solution {
 
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(String s) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        int totalCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String currentSubString = s.substring(i, j);
+
+                char[] chars = currentSubString.toCharArray();
+                Arrays.sort(chars);
+                currentSubString = String.valueOf(chars);
+
+                if (map.containsKey(currentSubString)) {
+
+                    int value = map.get(currentSubString);
+                    totalCount = totalCount + value;
+
+                    map.put(currentSubString, value + 1);
+                } else {
+                    map.put(currentSubString, 1);
+                }
+            }
+        }
+        return totalCount;
+
+    }
 
     private static final Scanner scanner = new Scanner(System.in);
 
