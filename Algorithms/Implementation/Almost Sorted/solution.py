268c679233be56843e00e0accf3edfe915d48513
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 17:47:24 +0530

    Update solution.py

--- Algorithms/Implementation/Almost Sorted/solution.py
+++ Algorithms/Implementation/Almost Sorted/solution.py
@@ -15,4 +15,10 @@ else:
     if len(wrong) == 2:
         print("yes")
         print("swap", wrong[0]+1, wrong[1]+1)
-    
+    else:           
+        index = sorted_array.index(array[wrong[0]])+1
+        if sorted_array[wrong[0]:index] == list(reversed(array[wrong[0]:index])):
+            print("yes")
+            print("reverse", wrong[0]+1, index)
+        else:
+            print("no")
