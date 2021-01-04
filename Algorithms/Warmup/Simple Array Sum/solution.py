1524b558933f2a4a6c475c2a517b8506114edf2c
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 11:27:33 +0530

    comments

--- Algorithms/Warmup/Simple Array Sum/solution.py
+++ Algorithms/Warmup/Simple Array Sum/solution.py
@@ -3,6 +3,7 @@ n = int(input())
 # input array elements
 nums = list(map(int, input().split()))
 sum = 0
+# Print the sum of the array's elements as a single integer
 for num in nums:
     sum += num
 print(sum)
