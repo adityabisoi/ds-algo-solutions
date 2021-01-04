61d1fe58cbfcd6c5114f728dcb56da9b43f8b59f
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 07:21:12 +0530

    Update solution.py

--- Algorithms/Game Theory/Zero-Move Nim/solution.py
+++ Algorithms/Game Theory/Zero-Move Nim/solution.py
@@ -1,3 +1,9 @@
 import sys
 from functools import reduce
 g = int(input().strip())
+
+for a0 in range(g):
+    n = int(input().strip())
+    p = [int(p_temp) for p_temp in input().strip().split(' ')]
+    res = 0
+    
