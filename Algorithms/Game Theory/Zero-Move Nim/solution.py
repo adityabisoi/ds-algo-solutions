637ef6f932421ab0176fe5ee9c71d92400b39f4c
yashasvimisra2798 <54177363+yashasvimisra2798@users.noreply.github.com>
2021-01-04 07:21:28 +0530

    Update solution.py

--- Algorithms/Game Theory/Zero-Move Nim/solution.py
+++ Algorithms/Game Theory/Zero-Move Nim/solution.py
@@ -6,4 +6,12 @@ for a0 in range(g):
     n = int(input().strip())
     p = [int(p_temp) for p_temp in input().strip().split(' ')]
     res = 0
+    for el in p:
+        gx = el - 1 if el % 2 == 0 else el + 1
+        if res == 0:
+            res = gx
+        else:
+            res = res ^ gx
     
+    john = res != 0
+    print('W' if john else 'L')
